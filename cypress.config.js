0e0069f0fa7fd297be8774ca3641b6fed27aca87
Abhishek kumar Arya <abhishekkumararya@Abhisheks-MacBook-Air.local>
2026-02-18 00:45:50 +0530

    added cypress-moch-awosome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     reportDir: "cypress/reports",
+    screenshotsFolder: "cypress/screenshots",
     overwrite: false,
     html: false,
     json: true,
